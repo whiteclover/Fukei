74746695d26f5e0946190de9aeeedbf7d13ce4d9
thomashuang <lyanghwy@gmail.com>
2013-12-16 21:18:33 +0800

    tests: fixed the indent

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 
 import unittest
-#!/usr/bin/env python
 from test_crypto import TestCrypto
-
+from fukei.utils import log_config
 
 if __name__ == '__main__':
-	from fukei.utils import log_config
     log_config('test', True)
     unittest.main(verbosity=2)
