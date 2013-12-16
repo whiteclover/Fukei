f3f15a2dc5b3f3ba5dd18945890302e93df4c488
thomashuang <lyanghwy@gmail.com>
2013-12-16 20:44:30 +0800

    tests: fixed pep8 style

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,11 @@
-import unittest
+#!/usr/bin/env python
 
+import unittest
+#!/usr/bin/env python
 from test_crypto import TestCrypto
 
 
 if __name__ == '__main__':
 	from fukei.utils import log_config
-	log_config('test', True)
-	unittest.main(verbosity=2)
+    log_config('test', True)
+    unittest.main(verbosity=2)
